58cf3463de89079ac2624eb1c24fd83335dfb737
husainleel <73138338+husainleel@users.noreply.github.com>
2021-09-12 12:27:20 +0300

    trail 2

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-console.log("hello");
+console.log("hello   jiji");
 ReactDOM.render(<App />, document.getElementById("root"));
